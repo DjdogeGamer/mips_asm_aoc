30d9e4d18b5f261cc3e31fb982c52ad15d46e4e7
Gustavo Henrique <ghcruz@motorola.com>
2022-10-26 11:32:41 -0300

    questao do boletim A OKe B, TODO: Questao B

--- boletim.asm
+++ boletim.asm
@@ -4,15 +4,96 @@
 
 
 .data 
-	# area: Dados da memoria principal
+
+	zero: .float 0.0
+	newline: .asciiz "\n"
+	msg1: .asciiz "Digite a primeira nota: "
+	msg2: .asciiz "Digite a segunda nota: "
+	msg3: .asciiz "Media: "
+	msg4: .asciiz "Digite a frequencia: "
+	msg5: .asciiz "Frequencia: "
+	msg6: .asciiz "Aprovado"
+	msg7: .asciiz "Reprovado"
+	const: .float 2.0
+	minFreq: .float 75.0
+	minMed: .float 7.0
 	
 .text
-	# area: Instrucoes do programa
+	main:
+		li $v0, 4 
+		la $a0, msg1 #"Digite a primeira nota: "
+		syscall 
 
-
+		li $v0, 6 # lê nota 1
+		syscall
 	
-	syscall # executa
+		mov.s $f1, $f0 #nota 1 para o $f1
+	
+		li $v0, 4 
+		la $a0, msg2 #"Digite a segunda nota: "
+		syscall 
 
-	li $v0, 10 # encerra programa 
-	syscall
-
+		li $v0, 6 # lê nota 2
+		syscall
+	
+		mov.s $f2, $f0 #nota 2 para o $f2
+	
+		#Calculo da media
+		add.s $f3, $f1, $f2
+		lwc1 $f4, const
+		div.s $f5, $f3, $f4
+	
+		mov.s $f12, $f5
+	
+		li $v0, 4 
+		la $a0, msg3 #"Media: "
+		syscall
+	
+		li $v0, 2 #imprime media
+		syscall
+	
+		li $v0, 4
+		la $a0, newline
+		syscall # "\n"
+	
+		li $v0, 4 
+		la $a0, msg4 #"Digite a frequencia: "
+		syscall
+	
+		li $v0, 6 # lê frequencia
+		syscall
+	
+		mov.s $f6, $f0 #frequencia para $f6
+		mov.s $f12, $f6
+	
+		li $v0, 4 
+		la $a0, msg5 #"Frequencia: "
+		syscall
+	
+		li $v0, 2 #imprime frequencia
+		syscall
+		
+		li $v0, 4
+		la $a0, newline
+		syscall # "\n"
+	
+		lwc1 $f7, minFreq #Minimo necessario frequencia
+		lwc1 $f8, minMed #Minimo necessario media
+	
+		c.lt.s $f6, $f7
+		bc1t reprovado
+		
+		c.lt.s $f5, $f8
+		bc1t reprovado
+		
+		li $v0, 4 
+		la $a0, msg6 #"Aprovado"
+		syscall
+	
+		li $v0, 10 # encerra programa 
+		syscall
+	
+	reprovado:
+		li $v0, 4 
+		la $a0, msg7 #Reprovado
+		syscall
